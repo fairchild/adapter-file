c2fa82c4d2401943cfb906604f515abf9f5ec2d4
Michael <fairchild@stimble.net>
2012-03-11 16:22:21 -0700

    freshly greated bundle gem adapter-file

---
+++ lib/adapter-file.rb
@@ -0,0 +1,7 @@
+require "adapter-file/version"
+
+module Adapter
+  module File
+    # Your code goes here...
+  end
+end
---
+++ lib/adapter-file/version.rb
@@ -0,0 +1,5 @@
+module Adapter
+  module File
+    VERSION = "0.0.1"
+  end
+end
