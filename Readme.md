7d1b7b0b9faea7f5dab300e9fd1adf24f2d7f300
Michael <fairchild@stimble.net>
2012-03-11 22:36:19 -0700

    remote debugging puts.

--- Readme.md
+++ Readme.md
@@ -1,1 +1,4 @@
 A keyvalue adapter that uses the filesytem to store data.
+
+extends https://github.com/jnunemaker/adapter with adapter/file
+
--- lib/adapter/file.rb
+++ lib/adapter/file.rb
@@ -2,23 +2,14 @@
 require 'adapter'
 require 'find'
 require 'fileutils'
+require 'adapter/file/version'
 
 module Adapter
   module File
 
-    def initialize(client, options={})
-     base_directory(client)  # this will be the base directory
-     paths = ::File.split(key)
-     FileUtils.mkdir_p(paths.first) unless ::File.directory?(paths.first)
-   end
-           
-    def base_directory(root='./')
-      @base_directory ||= ::File.expand_path(client)
-    end
-    
-    def full_key(key)
-      raise "you are not allowed to use double dot '..' notation in paths" if key.match(/\.\.\//)
-      ::File.expand_path(::File.join(base_directory, key))
+    def initialize(client='./files', options={})
+      FileUtils.mkdir_p(client)# unless ::File.directory?(client)
+      base_directory client  # this will be the base directory
     end
     
     # does the file already exist?
@@ -35,9 +26,19 @@ module Adapter
       files
     end
 
+    #return the file contents, or the directory listing if a directory
     def read(key)
-      ::File.read(full_key(key))
+      if ::File.file?(full_key(key))
+        ::File.read(full_key(key))
+      elsif  ::File.directory?(full_key(key))
+        Dir.entries('.')[2..-1]  #don't return the . and .. directories
+      end
     end
+    
+    #TODO implement fetch
+    # def fetch(key)
+    #   raise "not implemented yet.  will return value or passed default"
+    # end
 
     def write(key, value)
       paths = ::File.split(full_key(key))
@@ -45,19 +46,34 @@ module Adapter
       f = ::File.open(full_key(key), 'w') {|f| f.write(value) }
     end
 
-    def delete(key, options={:recursive=>false})      
-      if ::File.file?(full_key(key))
-        ::File.delete(full_key(key))
-      elsif ::File.directory?(full_key(key))
-        ::FileUtils.rm(full_key(key)) unless options[:recursive]
-        ::FileUtils.rm_r(full_key(key)) if options[:recursive]
-      else
-        raise 'unknown error'
+    def delete(key, options={:recursive=>false})
+      if key?(key)
+        value=read(key)
+        if ::File.file?(full_key(key))
+          ::File.delete(full_key(key))
+        elsif ::File.directory?(full_key(key))
+          if options[:recursive]
+          keys.each{|f| ::FileUtils.rm_r(full_key(f)) }
+          else
+            ::FileUtils.rm(full_key(key)) unless options[:recursive]
+          end
+        elsif !::File.directory?(full_key(key))
+          true
+        else
+          raise 'unknown error'
+        end
+        value
       end
     end
 
     def clear
-      delete('', {:recursive=>true})
+       delete('', {:recursive=>true}) 
+      # if ::File.directory?(base_directory)
+      #   delete('', {:recursive=>true}) 
+      # else
+      #   p [:client, client]
+      #   p [:base_dir, base_directory,  ::File.directory?(base_directory)]
+      # end
     end
 
     def encode(value)
@@ -68,6 +84,17 @@ module Adapter
       value
     end
 
+    
+    # private
+    def base_directory(root='./')
+      @base_directory ||= ::File.expand_path(client)
+    end
+    
+    def full_key(key)
+      raise "you are not allowed to use double dot '..' notation in paths" if key.match(/\.\.\//)
+      ::File.expand_path(::File.join(base_directory,key_for(key)))
+    end
+    
     Adapter.define(:file, Adapter::File)
     
   end
